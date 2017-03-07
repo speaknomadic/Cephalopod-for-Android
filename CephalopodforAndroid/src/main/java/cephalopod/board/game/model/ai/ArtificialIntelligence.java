35ff8d5e5cfb1412e2f8a5b9b635cada8053c9da
aleksandvc <aleksandvc@mail.bg>
2017-03-07 19:31:15 +0200

    Comments added

--- CephalopodforAndroid/src/main/java/cephalopod/board/game/model/ai/ArtificialIntelligence.java
+++ CephalopodforAndroid/src/main/java/cephalopod/board/game/model/ai/ArtificialIntelligence.java
@@ -2,7 +2,9 @@ package cephalopod.board.game.model.ai;
 
 import cephalopod.board.game.model.Cell;
 
-//TODO Add Java Doc comments.
+/**
+ * An interface which the AI algorithms implement.
+ */
 public interface ArtificialIntelligence {
     public int[] move(Cell cells[][], Cell.Type player) throws ImpossibleMoveException;
 }
