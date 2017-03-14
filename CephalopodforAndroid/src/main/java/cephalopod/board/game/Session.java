3f3aa712237c76e415f56836bcecdbed3221c594
aleksandvc <aleksandvc@mail.bg>
2017-03-14 11:05:42 +0200

    Session added

--- CephalopodforAndroid/src/main/java/cephalopod/board/game/Session.java
+++ CephalopodforAndroid/src/main/java/cephalopod/board/game/Session.java
@@ -1,8 +1,30 @@
 package cephalopod.board.game;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by vladimircvetanov on 11.03.17.
  */
 
 public class Session {
+
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
+    Context ctx;
+
+    public Session(Context ctx) {
+        this.ctx = ctx;
+        prefs = ctx.getSharedPreferences("cephalopod",Context.MODE_PRIVATE);
+        editor = prefs.edit();
+    }
+
+    public void setLoggedin(boolean loggedin){
+        editor.putBoolean("loggedInmode",loggedin);
+        editor.commit();
+    }
+
+    public boolean loggedIn(){
+        return prefs.getBoolean("loggedInmode",false);
+    }
 }
