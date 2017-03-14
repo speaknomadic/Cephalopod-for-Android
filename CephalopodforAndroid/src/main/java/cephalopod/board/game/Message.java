f47e686b523276e597e218e602a3afce9b0a924e
aleksandvc <aleksandvc@mail.bg>
2017-03-14 11:05:17 +0200

    Class for messages added

--- CephalopodforAndroid/src/main/java/cephalopod/board/game/Message.java
+++ CephalopodforAndroid/src/main/java/cephalopod/board/game/Message.java
@@ -1,8 +1,15 @@
 package cephalopod.board.game;
 
+import android.content.Context;
+import android.widget.Toast;
+
 /**
  * Created by vladimircvetanov on 13.03.17.
  */
 
 public class Message {
+
+    public static void message(Context context, String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
 }
