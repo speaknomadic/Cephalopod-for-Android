dda006a138e9bb0bcab84fd78a7ef5c7decd6a71
aleksandvc <aleksandvc@mail.bg>
2017-03-18 09:41:39 +0200

    About page added

--- CephalopodforAndroid/src/main/java/cephalopod/board/game/AboutActivity.java
+++ CephalopodforAndroid/src/main/java/cephalopod/board/game/AboutActivity.java
@@ -1,13 +1,17 @@
 package cephalopod.board.game;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * About the product screen.
  */
 public class AboutActivity extends Activity {
 
+    private Button back;
     /**
      * {@inheritDoc}
      */
@@ -15,5 +19,17 @@ public class AboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        back = (Button)findViewById(R.id.aboutBack_button);
+
+        View.OnClickListener listener= new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AboutActivity.this,GameActivity.class));
+            }
+        };
+
+        back.setOnClickListener(listener);
     }
 }
